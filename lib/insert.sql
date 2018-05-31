40537af9a3cc43b338fa8c0a18617f94ab920bc9
Alex Wilson <fonzmp@gmail.com>
2018-05-31 23:31:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Angry", "True")
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (age, gender, color, temperament, alive) VALUES (4, "M", "Brown", "Angry", "True");
