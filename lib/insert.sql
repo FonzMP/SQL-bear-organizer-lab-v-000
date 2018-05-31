730ce3beb392a1275b17c0b28f600ad4c9c38b18
Alex Wilson <fonzmp@gmail.com>
2018-05-31 23:32:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "
 INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Black", "Angry", "True");
 INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Brown", "Angry", "True");
 INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "Black", "Angry", "True");
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "M", "Brown", "Angry", "True");
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "F", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "M", "Brown", "Hungry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "F", "Brown", "Sad", "True");
 INSERT INTO (age, gender, color, temperament, alive) VALUES (3, "F", "Brown", "Really Angry", "True");
