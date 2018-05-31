7536b8d3671805716ecc54fefad370ecee5e9410
Alex Wilson <fonzmp@gmail.com>
2018-05-31 23:30:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Angry", true)
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Angry", "True")
