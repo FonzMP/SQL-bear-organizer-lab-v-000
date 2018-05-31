20053699cf4ab7920f398ceeeea5200cf47c64bd
Alex Wilson <fonzmp@gmail.com>
2018-05-31 23:32:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Angry", "True");
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "Rambunctious", "True");
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "Sad", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Black", "Angry", "True");
 INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Brown", "Angry", "True");
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "Black", "Angry", "True");
 INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "M", "Brown", "Angry", "True");
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "M", "Brown", "Angry", "True");
-INSERT INTO (age, gender, color, temperament, alive) VALUES (3, "M", "Brown", "Angry", "True");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "F", "Brown", "Angry", "True");
+INSERT INTO (age, gender, color, temperament, alive) VALUES (3, "F", "Brown", "Really Angry", "True");
